c50a330ea84f26dd8a1e29eeb696d352c6c331c0
Vanderson Nunes <vann.nunes@gmail.com>
2023-11-06 20:23:00 -0300

    Added terraform backend

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "mongodbatlas_advanced_cluster" "atlas-cluster" {
 
 # Stores variables into AWS ssm
 resource "aws_ssm_parameter" "mongodb_database_url" {
-  name        = "/${var.environment}/${var.application_tag_name}/MONGODB_URI"
+  name        = "/${var.application_tag_name}/${var.environment}/MONGODB_URI"
   description = "Auth Mongo DB Password"
   type        = "SecureString"
   value       = "mongodb+srv://${var.database_username}:${mongodbatlas_database_user.db-user.password }@${replace(mongodbatlas_advanced_cluster.atlas-cluster.connection_strings.0.standard_srv, "mongodb+srv://", "")}"
@@ -85,32 +85,11 @@ resource "aws_ssm_parameter" "mongodb_database_url" {
 
 # Stores variables into AWS ssm
 resource "aws_ssm_parameter" "mongodb_username" {
-  name        = "/${var.environment}/${var.application_tag_name}/MONGODB_DATABASE"
+  name        = "/${var.application_tag_name}/${var.environment}/MONGODB_DATABASE"
   description = "Database name"
   type        = "String"
   value       = var.database_name
   depends_on = [ random_password.db-user-password ]
-}
-
-# Creates a S3 Bucket to hold terraform tfstate file 
-resource "aws_s3_bucket" "auth_infra_bucket" {
-  bucket = tech_challenge_auth
-}
-
-# Bucket Ownership Controls
-resource "aws_s3_bucket_ownership_controls" "auth_infra_bucket_owner" {
-  bucket = aws_s3_bucket.auth_infra_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-#Bucket ACL
-resource "aws_s3_bucket_acl" "auth_infra_bucket_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.auth_infra_bucket_owner]
-
-  bucket = aws_s3_bucket.auth_infra_bucket.id
-  acl    = "private"
 }
 
 
@@ -118,7 +97,6 @@ resource "aws_s3_bucket_acl" "auth_infra_bucket_acl" {
 output "atlas_cluster_connection_string" { value = mongodbatlas_advanced_cluster.atlas-cluster.connection_strings.0.standard_srv }
 output "project_name"      { value = mongodbatlas_project.atlas-project.name }
 output "username"          { value = mongodbatlas_database_user.db-user.username } 
-output "bucket"            { value = aws_s3_bucket.auth_infra_bucket.s3_bucket_bucket_domain_name}
 output "user_password"     { 
   sensitive = true
   value = mongodbatlas_database_user.db-user.password 
